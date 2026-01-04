04c65ed52d8564aca0336cb32d7029e315f6b48f
點點 <a98g072@gmail.com>
2026-01-04 11:23:40 +0800

    錯誤修

--- index.tsx
+++ index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter as Router } from 'react-router-dom';
-import App from './App';
-import { ProjectProvider } from './context/ProjectContext';
+import App from './App.tsx';
+import { ProjectProvider } from './context/ProjectContext.tsx';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
